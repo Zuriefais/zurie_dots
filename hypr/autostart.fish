391d683b6529d0def839fe76b4b904c83995d94e
Zuriefais <74775691+Zuriefais@users.noreply.github.com>
2024-10-27 21:14:17 +0300

    Update waybar and hypr config

--- hypr/autostart.fish
+++ hypr/autostart.fish
@@ -1,12 +1,24 @@
 function discord
     hyprctl dispatch workspace 1
-    /opt/ArmCord/armcord & set discord_pid $last_pid &
-    echo $discord_pid
+    hyprctl dispatch focusmonitor HDMI-A-1
+    hyprctl dispatch exec [workspace 1 silent] legcord -- --enable-features=UseOzonePlatform --ozone-platform=wayland
+    sleep 5
 end
 
-discord &
-fish /home/zuriefais/Pictures/auto_wallpaper.fish &
-hyprctl dispatch exec [workspace 2 silent] telegram-desktop &
-hyprctl dispatch exec [workspace 3 silent] google-chrome &
-hyprctl dispatch exec [workspace 4 silent] kitty zellij &
+function chrome
+    spoof-dpi &
+    #--disable-gpu-compositing
+    hyprctl dispatch exec [workspace 3 silent] google-chrome-unstable -- --enable-features=UseOzonePlatform --ozone-platform=wayland
+end
+
+wpaperd &
+discord
+hyprctl dispatch focusmonitor DP-3
+hyprctl dispatch exec [workspace 2 silent] ayugram-desktop
+#hyprctl dispatch exec [workspace 2 silent] skypeforlinux
+chrome
+#steam -silent &
 blueman-applet &
+transmission-gtk -m &
+wl-paste --watch cliphist store &
+swayosd-server &
